32a56afc4f4039b8d355864bcf0d05fbed17664a
meitasulistyani <126336580+meitasulistyani@users.noreply.github.com>
2023-04-27 09:59:19 +0700

    39 HTTP Method POST
    Task for HTTP Method POST

---
+++ create-user.test.cy.js
@@ -0,0 +1,12 @@
+describe('Create New User',()=>{
+  it('Successfully create new user',()=>{
+    var user={
+        "name":"Eduwork",
+        "job":"Education"
+    }
+    cy.request('POST', 'https://reqres.in/api/users', user).then((response)=>{
+        expect(response.status).equal(201)
+        expect('name')
+    })
+  });
+});
